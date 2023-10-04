5df2c274bbe7fcb50d47885121c1d322449b8c02
fjoeda <f.yudanto@gmail.com>
2023-10-04 17:18:48 +0700

    update : add language composition chart

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,6 @@ RUN apt-get update && apt-get install -y ffmpeg libsm6 libxext6 wget
 COPY . /code/
 
 RUN pip3 install -r requirements.txt
-RUN wget -P /home/.cache/torch/hub/checkpoints/ https://download.pytorch.org/models/vgg19-dcbb9e9d.pth
+# RUN wget -P /home/.cache/torch/hub/checkpoints/ https://download.pytorch.org/models/vgg19-dcbb9e9d.pth
 
 ENTRYPOINT ["streamlit", "run", "app.py", "--server.port=8080", "--server.address=0.0.0.0"]
--- ui/experiments/word_level_lang_id.py
+++ ui/experiments/word_level_lang_id.py
@@ -1,20 +1,31 @@
 import streamlit as st
 from annotated_text import annotated_text
 from libs.shared.text.lang_id import LangIdCRF
+import plotly.express as px
 
 tagger = LangIdCRF()
 
 
 def word_level_lang_id_ui():
     st.title("Word Level Language Identification")
-    # st.divider()
-    # st.markdown("""
-
-    # """)
-    # st.divider()
-
     text_input = st.text_area("Input text")
-    st.write("Tagged result")
+    all_tagged = []
+    lang_list = {}
+
     for item in text_input.split("\n"):
         tagged = tagger.tag(item)
+        all_tagged.append(tagged)
+        for tag in tagged:
+            if tag[1] not in lang_list.keys():
+                lang_list[tag[1]] = 1
+            else:
+                lang_list[tag[1]] += 1
+
+    pie_plot = px.pie(names=lang_list.keys(), values=lang_list.values())
+
+    st.write("#### Language composition")
+    st.plotly_chart(pie_plot)
+
+    st.write("#### Tagged result")
+    for tagged in all_tagged:
         annotated_text(tagged)
